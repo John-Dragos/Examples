a1702dc6acba3d52fd22283efa42ca14d9bd7456
John Dragos <jd717322@ohio.edu>
2024-01-30 18:29:31 -0500

    testing

---
+++ gittest.cc
@@ -0,0 +1,20 @@
+/**
+ *        @file: gittest.cc
+ *      @author: John Dragos
+ *        @date: January 30, 2024
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///function prototypes
+
+int main(int argc, char const *argv[]) {
+
+   cout << "test this ho" << endl;
+   
+   return 0;
+}// main
